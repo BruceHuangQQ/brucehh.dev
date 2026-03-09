4c85ff28db4d0d2a3214ff7a3fb35fdc5e9e4408
BruceHuangQQ <brucewonghh@outlook.com>
2026-03-09 14:35:40 +1100

    Add dot pattern to background

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,25 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <UContainer class="sm:border-x border-default pt-10">
-      <AppHeader :links="navLinks" />
-      <slot />
-      <AppFooter />
-    </UContainer>
+  <div
+    class="min-h-screen"
+    style="
+      background-image: radial-gradient(
+        circle,
+        rgba(120, 120, 120, 0.4) 1px,
+        transparent 1px
+      );
+      background-size: 24px 24px;
+    "
+  >
+    <div
+      class="min-h-screen w-full bg-default sm:mx-auto sm:max-w-[var(--ui-container)] sm:border-x sm:border-default"
+    >
+      <UContainer class="pt-10">
+        <AppHeader :links="navLinks" />
+        <slot />
+        <AppFooter />
+      </UContainer>
+    </div>
   </div>
 </template>
